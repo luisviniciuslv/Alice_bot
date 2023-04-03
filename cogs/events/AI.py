0428d3251ae716f7dc011c8089eb484fb186e9c8
Luis Vinicius <luisvpersy@gmail.com>
2023-04-02 23:59:27 -0300

    chore: codify

--- cogs/events/AI.py
+++ cogs/events/AI.py
@@ -28,7 +28,7 @@ class AI(commands.Cog):
         if "!clear" in message.content.lower():
             return
         try:
-            if ("alice-ia" in message.channel.name) and (message.author.id != 985969436001439834):
+            if ("alice-ia" in message.channel.name or "1090827669882556487" in message.channel.id) and (message.author.id != 985969436001439834) and message.content.startswith('|') == True:
                 pensando = await message.channel.send('pensando...')
                 response = gpt3(message.content)
                 n = 2000
